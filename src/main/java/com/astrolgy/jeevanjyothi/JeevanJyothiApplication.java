8fc821291e14b1a12e028a1393d8a0b9a4e69c7c
Lakshmisagar <lakshmisagar16@gmail.com>
2020-08-18 22:23:27 -0400

    Initial commit.

---
+++ src/main/java/com/astrolgy/jeevanjyothi/JeevanJyothiApplication.java
@@ -0,0 +1,13 @@
+package com.astrolgy.jeevanjyothi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeevanJyothiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeevanJyothiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/astrolgy/jeevanjyothi/JeevanJyothiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astrolgy.jeevanjyothi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeevanJyothiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
